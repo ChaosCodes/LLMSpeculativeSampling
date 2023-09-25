ae021ad4e3ab7eb1ae61069da96ec771b50ecd71
曾广韬 <zengguangtao@cengguangtaodeMacBook-Air.local>
2023-09-25 12:05:38 +0800

    add run.sh script

---
+++ run.sh
@@ -0,0 +1,6 @@
+python byte_llama.py \
+    --input "In May, Singapore athletes did the nation proud by hauling home 51 gold, 43 silver and 64 bronze medals from the Southeast Asia (Sea) Games in Cambodia." \
+    --target_model_name Llama-2-7b-chat-hf \
+    --approx_model_name ByteLlama-320M-preview \
+    --gamma 8 \
+    --benchmark
